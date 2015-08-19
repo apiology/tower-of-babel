272ecfc8793b2bde801c82372834f6def2d2b4bf
Vince Broz <vince@broz.cc>
2015-08-19 19:31:38 -0400

    Add ex04

---
+++ ex-04-exe.js
@@ -0,0 +1,8 @@
+import {PI, sqrt, square} from './ex-04-module';
+
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+
+console.log(PI);
+console.log(sqrt(+arg1));
+console.log(square(+arg2));
