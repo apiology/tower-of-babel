f67864c4e994f85c6380b400a7c38d2d4ebe7aa5
Vince Broz <vince@broz.cc>
2015-08-19 20:15:30 -0400

    Solve without temp variables

--- ex-07.js
+++ ex-07.js
@@ -1,9 +1,6 @@
-let evenOrOdd = +process.argv[2];
-let evenOrOddKey = evenOrOdd % 2 === 0 ? "even" : "odd";
-let sum = +process.argv[3] + evenOrOdd;
 let obj = {
-  [evenOrOddKey]: evenOrOdd,
-  [sum]: sum
+  [+process.argv[2] % 2 === 0 ? "even" : "odd"]: +process.argv[2],
+  [+process.argv[3] + +process.argv[2]]: +process.argv[3] + +process.argv[2],
 };
 
 console.log(obj);
